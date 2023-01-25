a08a39d80c7bdcb7351fab459f080a9b1a91e4fa
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2023-01-25 14:07:57 -0300

    improving button component

---
+++ packages/ui/components/Button/Button.styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { variants } from "@catppuccin/palette";
+
+const { macchiato } = variants;
+
+export const BaseButton = styled.button`
+  background: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 10px;
+  border-radius: 4px;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const PrimaryButton = styled(BaseButton)`
+  background: ${macchiato.blue.hex};
+  color: ${macchiato.mantle.hex};
+`;
+
+export const SecondaryButton = styled(BaseButton)`
+  border: 1px solid ${macchiato.blue.hex};
+  color: ${macchiato.blue.hex};
+`;
---
+++ packages/ui/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import {
+  ReactNode,
+  ButtonHTMLAttributes,
+  useCallback,
+  MouseEvent,
+  FC,
+} from "react";
+import { PrimaryButton, SecondaryButton } from "./Button.styles";
+
+type ButtonVariant = "primary" | "secondary";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant?: ButtonVariant;
+}
+
+const variantMapping: Record<ButtonVariant, FC<ButtonProps>> = {
+  primary: PrimaryButton,
+  secondary: SecondaryButton,
+};
+
+export const Button = ({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  ...props
+}: ButtonProps) => {
+  const handleOnClick = useCallback(
+    (e: MouseEvent) => {
+      if (onClick) {
+        onClick(e);
+      }
+    },
+    [onClick]
+  );
+
+  const CurrentVariant = variantMapping[variant];
+
+  return (
+    <CurrentVariant onClick={handleOnClick} type={type} {...props}>
+      {children}
+    </CurrentVariant>
+  );
+};
